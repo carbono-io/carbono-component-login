651ddf11b3758ade8e0e0c7afd5993c4739e6ca4
Daniel Moura <oxesoft@gmail.com>
2015-10-01 17:28:23 -0300

    daily progress

--- index.js
+++ index.js
@@ -4,98 +4,133 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var config     = require('./config.json');
 var etcd       = require('./service-register');
+var request    = require('request');
 
 var app = express();
 
 app.use(bodyParser.json());
 
-var fakeToken = 'dfsfksjnfb28eriewnrewkjnsdo39jdoi';
-var users = [];
+var PROTOCOL       = 'http://';
+var DB             = '/login';
+var COLLECTION     = '/users';
+var CRUDER_ADDRESS = PROTOCOL + config.cruder.serverAddress + ':' + config.cruder.serverPort + DB + COLLECTION;
+var fakeToken      = 'dfsfksjnfb28eriewnrewkjnsdo39jdoi';
 
 //----------------------------------------------------------------------
 
-app.get('/', function (request, response) {
-    response.send('You are doing it wrong! Try http://carbono.io/');
+app.get('/', function (request, res) {
+    res.send('You are doing it wrong! Try http://carbono.io/');
 });
 
 //----------------------------------------------------------------------
 
-app.post('/createuser', function (request, response) {
-    if (request.body.username && request.body.password) {
-        var found = false;
-        var item = 0;
-        for (item in users) {
-            if (users[item]['username'] == request.body.username) {
-                found = true;
-                break;
+app.post('/createuser', function (req, res) {
+    if (req.body.username && req.body.password) {
+        var url = CRUDER_ADDRESS + '?query={"username":"' + req.body.username + '"}';
+        request(url, function (error, response, body) {
+            var found = false;
+            var item = 0;
+            var message = '';
+            var statusCode = 400;
+            var user;
+            var created = false;
+            statusCode = response.statusCode;
+            if (statusCode == 200) {
+                user = JSON.parse(body);
+                if (user[0]._id) {
+                    found = true;
+                }
+            } else {
+                message = 'unexpected status ' + statusCode + ' from the cruder machine when requesting ' + url;
             }
-        }
-        if (!found) {
-            users.push({'username': request.body.username, 'password': request.body.password});
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify({'created': true, 'message': 'success'}));
-        } else {
-            users[item]['password'] = request.body.password;
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify({'created': false, 'message': 'password changed'}));
-        }
+            if (statusCode == 200 && !found) {
+                request({
+                    url: url,
+                    method: 'POST',
+                    json: true,
+                    body: {'username': req.body.username, 'password': req.body.password}
+                }, function (error, response, body) {
+                    var statusCode = response.statusCode;
+                    var created = false;
+                    var message = '';
+                    if (statusCode == 201 && body.ok == 1) {
+                        created = true;
+                        message = 'user created';
+                    } else {
+                        message = 'error creating user';
+                    }
+                    res.writeHead(statusCode, {'Content-Type': 'application/json'});
+                    res.write(JSON.stringify({'created': created, 'message': message}));
+                    res.end();
+                });
+            } else {
+                var message = '';
+                if (statusCode == 200) {
+                    message = 'password updated';
+                } else {
+                    message = 'unexpected status ' + statusCode + ' from the cruder machine when requesting ' + url;
+                }
+                res.writeHead(statusCode, {'Content-Type': 'application/json'});
+                res.write(JSON.stringify({'created': false, 'message': message}));
+                res.end();
+            }
+        });
     } else {
-        response.writeHead(400, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({'created': false}));
+        res.writeHead(400, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({'created': false}));
     }
-    response.end();
 });
 
 //----------------------------------------------------------------------
 
-app.post('/login', function (request, response) {
-    if (request.body.username && request.body.password) {
+app.post('/login', function (req, res) {
+    if (req.body.username && req.body.password) {
         var found = false;
         for (var item in users) {
-            if (users[item]['username'] == request.body.username && users[item]['password'] == request.body.password) {
+            if (users[item]['username'] == req.body.username && users[item]['password'] == req.body.password) {
                 found = true;
                 break;
             }
         }
         if (found) {
             console.log("found");
-            response.writeHead(200, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify({'token': fakeToken}));
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify({'token': fakeToken}));
         } else {
-            response.writeHead(400, {'Content-Type': 'application/json'});
-            response.write(JSON.stringify({'message': 'wrong username/password'}));
+            res.writeHead(400, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify({'message': 'wrong username/password'}));
         }
     } else {
-        response.writeHead(400, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({'message': 'wrong username/password'}));
+        res.writeHead(400, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({'message': 'wrong username/password'}));
     }
-    response.end();
+    res.end();
 });
 
 //----------------------------------------------------------------------
 
-app.post('/authenticated', function (request, response) {
-    if (request.body.token == fakeToken) {
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({'isAuthenticated': true}));
+app.post('/authenticated', function (req, res) {
+    if (req.body.token == fakeToken) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({'isAuthenticated': true}));
     } else {
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({'isAuthenticated': false}));
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({'isAuthenticated': false}));
     }
-    response.end();
+    res.end();
 });
 
 //----------------------------------------------------------------------
 
-app.post('/logout', function (request, response) {
-    if (request.body.token == fakeToken) {
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({'loggedout': true}));
+app.post('/logout', function (req, res) {
+    if (req.body.token == fakeToken) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({'loggedout': true}));
     } else {
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify({'loggedout': false}));
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify({'loggedout': false}));
     }
-    response.end();
+    res.end();
 });
 
 //----------------------------------------------------------------------
